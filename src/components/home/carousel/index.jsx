77626a2ccef76cd0976cbec86217888d84106d8f
shivam singh <shivam.singh@mobavenue.com>
2023-09-12 10:08:33 +0530

    carousel change

--- src/components/home/carousel/index.jsx
+++ src/components/home/carousel/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Carousel = () => {
   return (
-    <div>
-        Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10Lorem10
+    <div className="industriestypes__casestudies-slider" id="slider">
+      <input type="radio" name="slider" id="s1" className="radiobtn" checked />
+      <input type="radio" name="slider" id="s2" className="radiobtn" />
+      <input type="radio" name="slider" id="s3" className="radiobtn" />
+      <input type="radio" name="slider" id="s4" className="radiobtn" />
+      <input type="radio" name="slider" id="s5" className="radiobtn" />
+      <label for="s1" id="slide1" className="label1">
+        <div className="industriestypes__slider">
+          <img src="./images/aware-box.png" alt="aware-box" />
+        </div>
+      </label>
+      <label for="s2" id="slide2" className="label2">
+        <div className="industriestypes__slider">
+          <img src="./images/chicken-lover.png" alt="chicken-lover" />
+        </div>
+      </label>
+      <label for="s3" id="slide3" className="label3">
+        <div className="industriestypes__slider">
+          <img src="./images/green.png" alt="green" />
+        </div>
+      </label>
+      <label for="s4" id="slide4" className="label4">
+        <div className="industriestypes__slider">
+          <img src="./images/chicken-lover.png" alt="chicken-lover" />
+        </div>
+      </label>
+      <label for="s5" id="slide5" className="label5">
+        <div className="industriestypes__slider">
+          <img src="./images/green.png" alt="green" />
+        </div>
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
